5d136d59ed2861ed284b3592ed40f0f3ccebb67b
Dustin Hammack <rdhammack88@hotmail.com>
2017-08-23 17:10:59 -0500

    Add files via upload

---
+++ versions/scripts.js
@@ -0,0 +1,16 @@
+ $(document).ready(function() {
+   
+    $('#accordion').accordion({
+        collapsible: true,
+        /*heightStyle: "content"*/
+    });
+    
+    $('.tabs').tabs();
+    
+//     $('#htmlLink').click(function(e) {
+        
+//         e.preventDefault();
+        
+//         $('#html#htmlOverview').show();
+    // });
+ });
